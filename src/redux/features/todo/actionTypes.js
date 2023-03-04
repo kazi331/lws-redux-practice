c79f0d7628af5d9c80ac3958f137c3256de99601
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2023-03-04 22:39:20 +0600

    initialized

--- src/redux/features/todo/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const LOAD_TODOS = 'todos/loadTodos'
-export const ADD = 'todos/add'
-export const DELETE = 'todos/delete'
-export const TOGGLE = 'todos/toggle'
-export const COLOR = 'todos/color'
-export const COMPLETE_ALL = 'todos/completeAll'
-export const CLEAR_COMPLETED = 'todos/clearCompleted'
--- src/redux/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (store) => (next) => (action) => {
-    console.log('action', action);
-    console.log('store', store.getState());
-    next(action);
-}
-export default logger;
