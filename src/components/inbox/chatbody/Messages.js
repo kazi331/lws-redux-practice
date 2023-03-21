0be61524eef4dcd1365cce26be9b7a87c63a2955
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2023-03-21 13:38:37 +0600

    messages showing up

--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -1,27 +1,18 @@
+import { useSelector } from "react-redux";
 import Message from "./Message";
 
-export default function Messages() {
+export default function Messages({ messages }) {
+    const { email } = useSelector((state) => state.auth.user) || {};
     return (
         <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
             <ul className="space-y-2">
-                <Message justify="start" message="Hjdjd" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
+                {messages.map(message => {
+                    const { message: msg, id, sender } = message || {};
+                    const justify = sender.email === email ? "end" : "start"
+                    return <Message key={id} justify={justify} message={msg} />
+                })
+                }
+
             </ul>
         </div>
     );
--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,7 +1,11 @@
 import { apiSlice } from "../api/apliSlice";
 
 export const messagesApi = apiSlice.injectEndpoints({
-    endpoints: builder => {
-        // endpoints here
-    }
+    endpoints: builder =>( {
+        getMessages: builder.query({
+            query: (id) => `/messages?conversationId=${id}&_sort=timestamp&_order=desc&page=1&_limit=${process.env.REACT_APP_MESSAGES_LIMIT}`
+        })
+    })
 })
+
+export const {useGetMessagesQuery} = messagesApi;
