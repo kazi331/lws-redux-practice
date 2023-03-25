cb6b31c4dca3e61ff49676e20e722360c83994ce
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2023-03-25 13:22:39 +0600

    cors issue fixed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const auth = require("json-server-auth");
 const jsonServer = require("json-server");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -20,6 +21,7 @@ const rules = auth.rewriter({
 server.use(rules);
 server.use(auth);
 server.use(router);
+server.use(cors());
 
 server.listen(port, () => {
     console.log(`JSON Server is running - http://localhost:${port}`);
